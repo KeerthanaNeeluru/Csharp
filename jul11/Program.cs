1dd1a52ae92f76750d04cf307763ceaf42067417
keerthana <keerthananeeluru2001@gmail.com>
2023-07-11 09:36:17 +0530

    first commit

---
+++ jul11/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jul11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("welcome to c#");
+            //Console.ReadKey();
+
+
+            //ex2
+
+
+            /*string fname;
+            string lname;
+          *//*  Console.WriteLine("Enter the first Name");
+            fname=Console.ReadLine();
+            Console.WriteLine("Enter the first Name");
+           lname=Console.ReadLine() ;
+            Console.WriteLine("Welcome "+fname+" "+lname);
+            Console.ReadKey();*/
+
+            //ex3
+
+
+            /*string name;
+            int age;
+            char grade;
+            DateTime doj;
+            double salary;
+            Console.WriteLine("Enter Name");
+            name= Console.ReadLine();
+            Console.WriteLine("Enter Age");
+            age=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter grade");
+            grade=char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter date of joining");
+            doj=DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter salary");
+            salary=double.Parse(Console.ReadLine());
+            Console.WriteLine("Name : " + name + "\nAge : " + age + "\ngrade : " + grade + "\nDate of Joining : " + doj + "\nsalary : " + salary);
+            Console.ReadKey();*/
+
+
+            //ex4
+
+
+
+        }
+    }
+}
